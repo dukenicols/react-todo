f693c08631dd624f4bb5657f5de680561033b7b4
Nicolas Duque <dukenicols@gmail.com>
2017-03-06 01:20:05 -0500

    Making the To-Do List Interactive

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 class TodoItem extends React.Component {
   render() {
-    return <li style={{
-      textDecoration: this.props.done ? 'line-through' : 'none'
-    }}>{ this.props.name }</li>;
+    return <li>
+              <span style={{
+                textDecoration: this.props.done ? 'line-through' : 'none'
+              }}>{ this.props.name } </span>
+              <a href="#" onClick={this.props.onToggleDone}>{ this.props.done ? 'Mark as not done' : 'Mark as done' }</a>
+               &nbsp;
+              <a href="#" onClick={this.props.onRemove}>Remove</a>
+            </li>;
   }
 }
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
 
 import TodoItem from './TodoItem';
+import TodoInput from './TodoInput';
 
 class TodoList extends React.Component {
-  render() {
-    const items = [
-      { name: 'First item',done: false },
-      { name: 'Second item', done: true},
-      { name: 'Third item', done: false}
-    ];
+  constructor() {
+    super();
 
+    this.state = {
+      items: []
+    };
+  }
+
+  handleAddItem(name) {
+    const newItems = this.state.items.concat({ name: name, done: false });
+    this.setState({ items: newItems });
+  }
+
+  handleToggleDone(item) {
+    // slice() -> creates a new identical array
+    const newItems = this.state.items.slice();
+    newItems[newItems.indexOf(item)].done = !item.done;
+
+    this.setState({ items: newItems });
+  }
+
+  handleRemove(item) {
+    const newItems = this.state.items.slice();
+    delete newItems[newItems.indexOf(item)];
+
+    this.setState({ items: newItems });
+  }
+
+  render() {
     return (
-      <ul>
-        { items.map(item => <TodoItem name={item.name} done={item.done} />) }
-      </ul>
+      <div>
+        <ul>
+          { this.state.items.map(item => <TodoItem
+                                           name={item.name}
+                                           onRemove={this.handleRemove.bind(this, item)}
+                                           onToggleDone={this.handleToggleDone.bind(this, item)}
+                                           done={item.done} />) }
+        </ul>
+        <TodoInput onAddItem={this.handleAddItem.bind(this)} />
+      </div>
     );
   }
 }
